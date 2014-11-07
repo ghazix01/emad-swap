bbb854c3d00cda4a80a8d4205edaeb041f138563
Ghazi Raad <mohamadghaziraad@Ghazis-MacBook-Pro.local>
2014-11-07 09:29:01 +0200

    Initial Commit

---
+++ emad swap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  emad swap
+//
+//  Created by Ghazi Raad on 11/7/14.
+//  Copyright (c) 2014 Ghazi Raad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
